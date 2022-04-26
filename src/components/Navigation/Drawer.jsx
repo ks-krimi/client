752b14627930de2a67f1ec01dbb8b2b15e9a1292
Ks Krimi <ny.kalash@gmail.com>
2022-04-26 16:45:48 +0200

    🌱  (Drawer) add new links
    list links

--- src/components/Navigation/Drawer.jsx
+++ src/components/Navigation/Drawer.jsx
@@ -1,15 +1,32 @@
+import { useState } from 'react'
 import {
   Drawer as MDrawer,
   List,
   ListItem,
   ListItemIcon,
-  ListItemText
+  ListItemText,
+  Collapse,
+  Divider
 } from '@material-ui/core'
-import { Computer, Group, List as ListIcon, HowToReg } from '@material-ui/icons'
+import {
+  Computer,
+  Group,
+  List as ListIcon,
+  HowToReg,
+  ExpandLess,
+  ExpandMore,
+  MoreHoriz
+} from '@material-ui/icons'
 import { useHistory } from 'react-router-dom'
 
 function Drawer({ open, setOpen }) {
   const history = useHistory()
+
+  const [isOpen, setIsOpen] = useState(false)
+
+  const handleClick = () => {
+    setIsOpen(!isOpen)
+  }
 
   return (
     <MDrawer anchor="left" open={open} onClose={() => setOpen(false)}>
@@ -38,6 +55,42 @@ function Drawer({ open, setOpen }) {
               <ListItemText primary={array.name} />
             </ListItem>
           ))}
+        </List>
+        <Divider />
+        <List>
+          <ListItem button onClick={handleClick}>
+            <ListItemIcon>
+              <MoreHoriz />
+            </ListItemIcon>
+            <ListItemText primary="Plus" />
+            {isOpen ? <ExpandLess /> : <ExpandMore />}
+          </ListItem>
+          <Collapse in={isOpen} timeout="auto" unmountOnExit>
+            <List component="div" disablePadding>
+              {[
+                { name: 'Listes des materiels', path: 'list/materiel' },
+                { name: 'Listes des utilisateurs', path: 'list/user' },
+                { name: 'Listes des techniciens', path: 'list/technicien' }
+              ].map((array, index) => (
+                <ListItem
+                  button
+                  key={array.name}
+                  onClick={() => {
+                    history.push(array.path)
+                    setOpen(false)
+                  }}
+                >
+                  <ListItemIcon>
+                    {index === 0 && <Computer />}
+                    {/* {index === 1 && <ListIcon />} */}
+                    {index === 1 && <Group />}
+                    {index === 2 && <HowToReg />}
+                  </ListItemIcon>
+                  <ListItemText primary={array.name} />
+                </ListItem>
+              ))}
+            </List>
+          </Collapse>
         </List>
       </div>
     </MDrawer>
